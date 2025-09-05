332729a135d6450789968357fbe715f66e8871fe
brightvip <brightvip@msn.com>
2025-09-05 10:44:26 +0800

    rm wslx
    rm wslx

--- bin/Xray.sh
+++ bin/Xray.sh
@@ -50,40 +50,6 @@ cat << EOF >/usr/app/lib/Xray/XrayConfig.json.template
       }
     },
     {
-      "port": wsxport,
-      "listen": "wsxlisten",
-      "protocol": "wsxprotocol",
-      "settings": {
-        "clients": [
-          {
-            "id": "wsxCLIENTSID",
-            "level": 0,
-            "flow": "xtls-rprx-vision"
-          }
-        ],
-        "decryption": "wsxdecryption"
-      },
-      "streamSettings": {
-        "network": "ws",
-        "security": "none",
-        "wsSettings": {
-          "path": "WSXPATH",
-          "maxEarlyData": 1024,
-          "earlyDataHeaderName": "Sec-WebSocket-Protocol",
-          "acceptProxyProtocol": false
-        }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls",
-          "quic",
-          "fakedns"
-        ]
-      }
-    },
-    {
       "port": xhttpport,
       "listen": "xhttplisten",
       "protocol": "xhttpprotocol",
@@ -155,16 +121,6 @@ cat << EOF >/usr/app/lib/Xray/Xrayl.caddy.template
       		}
 	}
 
-	handle WSXPATH {
-    		@websocket {
-    			header Connection Upgrade
-    			header Upgrade websocket
-    		}
-    		reverse_proxy @websocket wsxlisten:wsxport {
-      			flush_interval -1
-      		}
-	}
-
 	handle XHTTPPATH {
     		reverse_proxy xhttplisten:xhttpport {
         		transport http {
@@ -180,8 +136,6 @@ EOF
  sync
  
  sed -e 's/wsport/9303/'  -e 's/wsprotocol/vless/' -e 's/wslisten/127.0.0.1/' -e 's:wsCLIENTSID:'"${CLIENTSID}"':'  -e 's:WSPATH:'"${PREFIX_PATH}/wsl/"':' \
-     -e 's/wsxport/9304/'  -e 's/wsxprotocol/vless/' -e 's/wsxlisten/127.0.0.1/' -e 's:wsxCLIENTSID:'"${CLIENTSID}"':'  -e 's:WSXPATH:'"${PREFIX_PATH}/wslx/"':' \
-     -e 's:wsxdecryption:'"${VLESS_ENCRYPTION}"':' \
      -e 's/xhttpport/9300/'  -e 's/xhttpprotocol/vless/' -e 's/xhttplisten/127.0.0.1/' -e 's:xhttpCLIENTSID:'"${CLIENTSID}"':'  -e 's:XHTTPPATH:'"${PREFIX_PATH}/xhttplx/"':'  \
 	 -e 's:xhttpdecryption:'"${VLESS_ENCRYPTION}"':' \
      /usr/app/lib/Xray/XrayConfig.json.template > /usr/app/lib/Xray/Xrayl.json
@@ -189,7 +143,6 @@ EOF
  sync
 
  sed -e 's/wsport/9303/' -e 's/wslisten/127.0.0.1/'  -e 's:WSPATH:'"${PREFIX_PATH}/wsl/*"':' \
-     -e 's/wsxport/9304/' -e 's/wsxlisten/127.0.0.1/'  -e 's:WSXPATH:'"${PREFIX_PATH}/wslx/*"':' \
      -e 's/xhttpport/9300/' -e 's/xhttplisten/127.0.0.1/'  -e 's:XHTTPPATH:'"${PREFIX_PATH}/xhttplx/*"':'  /usr/app/lib/Xray/Xrayl.caddy.template > /usr/app/lib/Xray/Xrayl.caddy
 
  sync
@@ -253,6 +206,7 @@ do
     start
     
 done
+
 
 
 
