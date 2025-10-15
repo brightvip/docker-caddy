e1aa937eede07f09fb31b14b3c84e14a9e1ddeb3
brightvip <brightvip@msn.com>
2025-10-15 16:06:09 +0800

    Update entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile echo 1 > /proc/sys/vm/overcommit_memory
+fallocate -l 512M /usr/app/bin/swapfile
+chmod 0600 /usr/app/bin/swapfile
+mkswap /usr/app/bin/swapfile
+swapon /usr/app/bin/swapfile
+echo 10 > /proc/sys/vm/swappiness
+echo 1 > /proc/sys/vm/overcommit_memory
