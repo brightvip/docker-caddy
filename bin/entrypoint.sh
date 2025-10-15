1d36956deff1669e48c06d56329923b2ea7e0b54
brightvip <brightvip@msn.com>
2025-10-15 16:15:03 +0800

    Delete bin/entrypoint.sh

--- bin/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fallocate -l 512M /usr/app/bin/swapfile
-chmod 0600 /usr/app/bin/swapfile
-mkswap /usr/app/bin/swapfile
-swapon /usr/app/bin/swapfile
-echo 10 > /proc/sys/vm/swappiness
-echo 1 > /proc/sys/vm/overcommit_memory
